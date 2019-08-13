83bf28bd468bdd95854371f24e44181b3c02f045
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-13 15:26:24 -0300

    Updates

---
+++ precache-manifest.e526d21d0bbab133f644782a1fc4767d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aeb8c80ee62ee9730daf1652238417f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "abacf1e4046586d1cb50",
+    "url": "/portfolio/static/js/2.6334a5ee.chunk.js"
+  },
+  {
+    "revision": "f89f0a4c78bc01e1cc75",
+    "url": "/portfolio/static/js/main.7ee24bb1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f4f7d0ee2eb3c3607a9374496a0a61ea.js"
+  "/portfolio/precache-manifest.e526d21d0bbab133f644782a1fc4767d.js"
 );
 
 self.addEventListener('message', (event) => {
