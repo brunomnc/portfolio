249ba1625a40c428650608c122adc4dae16a3dc9
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-13 12:37:32 -0300

    Updates

---
+++ precache-manifest.d8c82225cd957146a66299e6e3906787.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c6c2053a04228a41ccbf675fd2eec4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/portfolio/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "aa16983ee669d6ed064f",
+    "url": "/portfolio/static/js/main.549ed837.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
