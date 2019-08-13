5101480a9d6cf15db5a39d062ea1ba8ee49e5aa2
ZummyGummy <brunomnc@yahoo.com.br>
2019-08-13 19:39:46 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3745e024514652ada86ce6facbbc29fb.js"
+  "/portfolio/precache-manifest.fb285081a4f6b7f1bd6f688c4d20d159.js"
 );
 
 self.addEventListener('message', (event) => {
